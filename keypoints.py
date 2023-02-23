91d5cc472ef0d65243192f7b7a107fac19aaccfc
sc20dzi <sc20dzi@leeds.ac.uk>
2023-02-23 20:50:25 +0000

    improved data pre-processing

--- keypoints.py
+++ keypoints.py
@@ -1,15 +1,16 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import glob
-import os
+import cv2
 import numpy as np
 import pandas as pd
-import cv2
 import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from sklearn.model_selection import train_test_split
+import sklearn
+from sklearn import model_selection
+import glob
+import os
 
 # set up paths to folders
 def setup_paths ():
@@ -21,49 +22,80 @@ def setup_paths ():
     folders = ['CAT_00']
     return folder_dir, folders
 
-# load & normalise the images and their corresponding keypoints,
-def load_data():
-    
+# Load the images and their corresponding key points
+def load_data ():
     folder_dir, folders = setup_paths()
     images = []
     keypoints = []
-    
     for folder in folders:
         path = os.path.join(folder_dir, folder, '*.jpg')
         images_paths = sorted(glob.glob(path))
         
         for image_path in images_paths:
-            
-            # load the image
+            # Load the image
             img = cv2.imread(image_path)
             original_size = img.shape[:2]
-            
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             # load keypoints
             with open(image_path+'.cat', 'r') as f:
                 keypoints_text = f.read().strip()
+
+            # Parse the keypoints text
             keypoints_arr = np.array(keypoints_text.split(' ')[1:], dtype=np.float32)
             keypoints_arr = keypoints_arr.reshape(-1, 2)
 
-            # normalise the image and keypoints
+            # Basic pre processing:
+            # Resize the image and keypoints
             x_scale = 256 / img.shape[1]
             y_scale = 256 / img.shape[0]
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             image = cv2.resize(img, (256, 256))
             keypoints_arr = keypoints_arr * [x_scale, y_scale]
 
-            # normalise pixel values
+            # Normalize the image and keypoints
             image = image / 255.0
             keypoints_arr = keypoints_arr / 255.0
 
-            # add images and keypoints to lists
+            # Add the image and keypoints to the lists
             images.append(image)
             keypoints.append(keypoints_arr)
+            
+    return images, keypoints, original_size
 
-    # convert the lists to numpy arrays
+# pre-process the data
+def preprocess_data ():
+    images, keypoints, size = load_data()
+    
+    # Convert the lists to numpy arrays
     images = np.array(images)
     keypoints = np.array(keypoints)
+    
+    # reshape the arrays
+    images = np.expand_dims(images, axis=-1)
+    keypoints = keypoints.reshape(len(keypoints),18)
+    
+    # split the dataset into train and test sets
+    train_val_images, test_images, train_val_keypoints, test_keypoints = model_selection.train_test_split(
+        images, keypoints, test_size=0.2, random_state=42)
 
-    return images, keypoints, original_size
+    # split the train and validation sets
+    train_images, val_images, train_keypoints, val_keypoints = model_selection.train_test_split(
+        train_val_images, train_val_keypoints, test_size=0.2, random_state=42)
+    
+    # set up data generators for the sets
+    train_datagen = ImageDataGenerator(rescale=1./255)
+    val_datagen = ImageDataGenerator(rescale=1./255)
+    test_datagen = ImageDataGenerator(rescale=1./255)
+
+    train_generator = train_datagen.flow(
+        train_images, train_keypoints, batch_size=32)
+
+    validation_generator = val_datagen.flow(
+        val_images, val_keypoints, batch_size=32)
+    
+    test_generator = test_datagen.flow(
+        test_images, test_keypoints, batch_size=32)
+    
+    return train_generator, test_generator, validation_generator
 
 # display an image and its corresponding keypoint
 def display_image(images, keypoints, index, original_size):
@@ -83,7 +115,7 @@ def display_image(images, keypoints, index, original_size):
     plt.show()
 
 # define the CNN architecture - might improve later
-def create_model():
+def create_model ():
     model = tf.keras.Sequential([
         tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 1)),
         tf.keras.layers.MaxPooling2D((2, 2)),
@@ -98,7 +130,7 @@ def create_model():
     return model
 
 # run the code
-def main():
+def main ():
     
     # TEMPORARY - testing load_data() and display_image()
     images, keypoints, original_size = load_data()
