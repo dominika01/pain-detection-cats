15cf6b3a8bd4a778ab330db85e48dacbf29bcee2
sc20dzi <sc20dzi@leeds.ac.uk>
2023-03-07 18:44:22 +0000

    loading features  and their labels separately

--- detect_pain.py
+++ detect_pain.py
@@ -19,17 +19,17 @@ def preprocess_image(img):
     img = img / 255.0 #normalize pixel values
     return img
 
-# load and preprocess the data
-def load_data():
-    ## LABELS
-    # set up the path
-    csv_path = 'labels_preprocessed.csv'
+# load the labels into a dataframe
+def load_labels():
     # read data into a pandas dataframe
+    csv_path = 'labels_preprocessed.csv'
     labels = pd.read_csv(csv_path)
+    
     # replace NaN values with 0s
     labels.fillna(0)
-    
-    ## EARS
+  
+# load the images and labels of ears  
+def load_ears():
     # set up the path
     ears_path = 'data/ears'
     ears_dir = os.listdir(ears_path)
@@ -45,7 +45,14 @@ def load_data():
         x_ears = np.array(x_ears)
         x_ears = np.expand_dims(x_ears, axis=-1)
     
-    ## EYES
+    # load labels
+    labels = load_labels()
+    y_ears = labels.iloc [:, [0,1]]
+    
+    return x_ears, y_ears
+
+# load the images and labels of eyes 
+def load_eyes():
     # set up the path
     eyes_path = 'data/eyes'
     eyes_dir = os.listdir(eyes_path)
@@ -60,8 +67,15 @@ def load_data():
         x_eyes.append(preprocess_image(img))
         x_eyes = np.array(x_eyes)
         x_eyes = np.expand_dims(x_eyes, axis=-1)
-        
-    ## MUZZLE
+    
+    # load labels
+    labels = load_labels()
+    y_eyes = labels.iloc [:, [0,2]]
+    
+    return x_eyes, y_eyes
+    
+# load the images and labels of muzzles 
+def load_muzzle():
     # set up the path
     muzzle_path = 'data/muzzle'
     muzzle_dir = os.listdir(muzzle_path)
@@ -76,9 +90,56 @@ def load_data():
         x_muzzle.append(preprocess_image(img))
         x_muzzle = np.array(x_muzzle)
         x_muzzle = np.expand_dims(x_muzzle, axis=-1)
-        
-    return labels, x_ears, x_eyes, x_muzzle
+    
+    # load labels
+    labels = load_labels()
+    y_muzzle = labels.iloc [:, [0,3]]
+    
+    return x_muzzle, y_muzzle
 
+# load the images and labels of whiskers 
+def load_whiskers():
+    # set up the path
+    muzzle_path = 'data/muzzle'
+    muzzle_dir = os.listdir(muzzle_path)
+
+    # iterate over images
+    for image in muzzle_dir:
+        # load the image
+        image_path = os.path.join(muzzle_path, image)
+        img = cv2.imread(image_path)
+        # preprocess the image
+        x_muzzle = []
+        x_muzzle.append(preprocess_image(img))
+        x_muzzle = np.array(x_muzzle)
+        x_muzzle = np.expand_dims(x_muzzle, axis=-1)
+    
+    # load labels
+    labels = load_labels()
+    y_whiskers = labels.iloc [:, [0,4]]
+
+# load the images and labels of heads     
+def load_head():
+    # set up the path
+    head_path = 'data/head'
+    head_dir = os.listdir(head_path)
+
+    # iterate over images
+    for image in head_dir:
+        # load the image
+        image_path = os.path.join(head_path, image)
+        img = cv2.imread(image_path)
+        # preprocess the image
+        x_head = []
+        x_head.append(preprocess_image(img))
+        x_head = np.array(x_head)
+        x_head = np.expand_dims(x_head, axis=-1)
+    
+    # load labels
+    labels = load_labels()
+    y_head = labels.iloc [:, [0,5]]
+    
+    return x_head, y_head
 
 def split_data(x_data, y_data):
     # split the dataset into train and test sets
