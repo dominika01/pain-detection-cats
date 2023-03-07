db45c208de8ef33b659b4979c227a8c92581c280
sc20dzi <sc20dzi@leeds.ac.uk>
2023-03-07 12:45:00 +0000

    added some data preprocessing

--- detect_pain.py
+++ detect_pain.py
@@ -9,25 +9,76 @@ import tensorflow as tf
 from keras.metrics import RootMeanSquaredError
 from sklearn import model_selection
 
+INPUT_SHAPE = 256
 
 ### DATA PREPROCESSING
+# preprocess an individual image
+def preprocess_image(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert to gray
+    img = cv2.resize(img, (INPUT_SHAPE, INPUT_SHAPE)) #resize
+    img = img / 255.0 #normalize pixel values
+    return img
+
+# load and preprocess the data
 def load_data():
+    ## LABELS
     # set up the path
     csv_path = 'labels_preprocessed.csv'
     # read data into a pandas dataframe
-    df = pd.read_csv(csv_path)
-    return df
+    labels = pd.read_csv(csv_path)
+    # replace NaN values with 0s
+    labels.fillna(0)
+    
+    ## EARS
+    # set up the path
+    ears_path = 'data/ears'
+    ears_dir = os.listdir(ears_path)
 
-def preprocess_data():
-    # load data and replace NaN values with 0s
-    df = load_data()
-    df.fillna(0)
+    # iterate over images
+    for image in ears_dir:
+        # load the image
+        image_path = os.path.join(ears_path, image)
+        img = cv2.imread(image_path)
+        # preprocess the image
+        x_ears = []
+        x_ears.append(preprocess_image(img))
+        x_ears = np.array(x_ears)
+        x_ears = np.expand_dims(x_ears, axis=-1)
     
-    # split the data into x and y sets
-    x_data = df.iloc[:, :-1] # df without the last column
-    y_data = df.iloc[:, [0, -1]] # df only with the first and last column
-    
-    return x_data, y_data
+    ## EYES
+    # set up the path
+    eyes_path = 'data/eyes'
+    eyes_dir = os.listdir(eyes_path)
+
+    # iterate over images
+    for image in eyes_dir:
+        # load the image
+        image_path = os.path.join(eyes_path, image)
+        img = cv2.imread(image_path)
+        # preprocess the image
+        x_eyes = []
+        x_eyes.append(preprocess_image(img))
+        x_eyes = np.array(x_eyes)
+        x_eyes = np.expand_dims(x_eyes, axis=-1)
+        
+    ## MUZZLE
+    # set up the path
+    muzzle_path = 'data/muzzle'
+    muzzle_dir = os.listdir(muzzle_path)
+
+    # iterate over images
+    for image in muzzle_dir:
+        # load the image
+        image_path = os.path.join(muzzle_path, image)
+        img = cv2.imread(image_path)
+        # preprocess the image
+        x_muzzle = []
+        x_muzzle.append(preprocess_image(img))
+        x_muzzle = np.array(x_muzzle)
+        x_muzzle = np.expand_dims(x_muzzle, axis=-1)
+        
+    return labels, x_ears, x_eyes, x_muzzle
+
 
 def split_data(x_data, y_data):
     # split the dataset into train and test sets
@@ -69,5 +120,3 @@ def head():
 ### MAIN
 def main():
     pass
-
-preprocess_data()
