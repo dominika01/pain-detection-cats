6435f3d925ccb74fc3ae63a8c3ba43ff9f8660fe
sc20dzi <sc20dzi@leeds.ac.uk>
2023-03-18 20:48:49 +0100

    added a neural network

--- calculate_score.py
+++ calculate_score.py
@@ -7,6 +7,9 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.svm import SVC
 from sklearn.metrics import mean_squared_error, accuracy_score
 from sklearn.model_selection import cross_val_score, train_test_split, GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasRegressor
 
 
 # load the labels into a dataframe
@@ -202,17 +205,74 @@ def svm_grid_search(x_train, x_test, y_train, y_test):
     y_pred = best_model.predict(x_test)
     evaluate_model(best_model, x_test, y_test, y_pred)
     
+def neural_network(x_train, x_test, y_train, y_test):
+    # Define the model architecture
+    model = Sequential()
+    model.add(Dense(10, input_dim=x_train.shape[1], activation='relu'))
+    model.add(Dense(5, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+
+    # Compile the model
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    # Fit the model to the training data
+    model.fit(x_train, y_train, epochs=50, batch_size=32, validation_split=0.2)
+
+    # Evaluate the model on the test data
+    scores = model.evaluate(x_test, y_test)
+    y_pred = model.predict(y_test)
+    print(scores)
+
+def nn_grid_search(x_train, x_test, y_train, y_test):
+    model = Sequential()
+    model.add(Dense(12, input_dim=x_train.shape[1], activation='relu'))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(1, activation='linear'))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+
+    # Create the model
+    model = KerasRegressor(build_fn=model, verbose=0)
+
+    # Define the grid search parameters
+    batch_size = [10, 20, 40, 60, 80, 100]
+    epochs = [10, 50, 100]
+    param_grid = dict(batch_size=batch_size, epochs=epochs)
+
+    # Perform the grid search
+    grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=3)
+    grid_search = grid.fit(x_train, y_train)
+    
+    # select the best hyperparameters
+    best_params = grid_search.best_params_
+    best_model = grid_search.best_estimator_
+    print(best_params)
+    
+    # evaluate
+    y_pred = best_model.predict(x_test)
+    evaluate_model(best_model, x_test, y_test, y_pred)
+    
+    # round and convert predictions to integers
+    y_pred = np.round(y_pred,0)
+    y_pred = y_pred.astype(int)
+    
+    # calculate error
+    lin_mse = mean_squared_error(y_test, y_pred)
+    lin_rmse = np.sqrt(lin_mse)
+    print(lin_mse, lin_rmse)
+
+# run the code
 def main():
     x_train, x_test, y_train, y_test = get_data()
     print("Running the algorithm…")
+    
     #linear_regression(x_train, x_test, y_train, y_test)
     #logistic_regression(x_train, x_test, y_train, y_test)
     #tree_regressor(x_train, x_test, y_train, y_test)
     #random_forest(x_train, x_test, y_train, y_test)
     #forest_grid_search(x_train, x_test, y_train, y_test)
     #svm(x_train, x_test, y_train, y_test)
-    svm_grid_search(x_train, x_test, y_train, y_test)
+    #svm_grid_search(x_train, x_test, y_train, y_test)
+    neural_network(x_train, x_test, y_train, y_test)
     print("Done.\n")
-    
 
 main()
