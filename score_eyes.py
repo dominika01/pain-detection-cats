061565e3df597f999a56314b007f32bf30bd70d7
sc20dzi <sc20dzi@leeds.ac.uk>
2023-03-27 12:53:20 +0100

    used vgg16 and adjusted hyperparameters

--- score_eyes.py
+++ score_eyes.py
@@ -5,17 +5,19 @@ import os
 import pandas as pd
 import tensorflow as tf
 from sklearn import model_selection
+import matplotlib.pyplot as plt
+
+### UNDERSAMPLING APPROACH
 
 # global variables
 INPUT_SHAPE_X = 128
 INPUT_SHAPE_Y = 64
 ITERATION = '1'
-EPOCHS = 8
-BATCH_SIZE = 32
+EPOCHS = 250
+BATCH_SIZE = 8
 
-### ERROR: there are too many pictures somehow???
+### DATA PREPROCESSING
 
-### DATA PREPROCESSING - works
 # preprocess an individual image
 def preprocess_image(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert to gray
@@ -27,7 +29,7 @@ def preprocess_image(img):
 def load_labels():
     print("Loading labels…")
     # read data into a pandas dataframe
-    csv_path = 'labels_preprocessed.csv'
+    csv_path = 'data-labels/labels_preprocessed.csv'
     labels = pd.read_csv(csv_path)
     
     # replace NaN values with 0s
@@ -35,40 +37,74 @@ def load_labels():
     
     print("Done.\n")
     return labels
-
-# load the images and labels of eyes 
+  
+# load the images and labels of eyes  
 def load_eyes():
     # load labels
     labels = load_labels()
-    y_eyes = labels.iloc [:, 2]
-    x_eyes = []
-    check = []
     
     print("Loading eyes…")
     # set up the path
     eyes_path = 'data/eyes'
     eyes_dir = os.listdir(eyes_path)
+    
+    # init arrays
+    x_eyes = []
+    y_eyes = []
+    
+    # for undersampling purposes
+    max_images = 1000 # count of minority class
+    class_0 = 0
+    class_1 = 0
+    class_2 = 0
+    i = 0
+    
     # iterate over images
     for image in eyes_dir:
-        
-        check.append(image)
-        n = len(check)
-        for i in range (n-1):
-            if image == check[i]:
-                print("ALERT FOR", image)
-        
         # load the image
-        image_path = os.path.join(eyes_path, image) # changed eyes_dir to eyes_path
+        image_path = os.path.join(eyes_path, image)
         img = cv2.imread(image_path)
         
         # preprocess the image
         if (img is not None):
-            x_eyes.append(preprocess_image(img))
-
-    x_eyes = np.array(x_eyes)
-    x_eyes = x_eyes.reshape(-1, INPUT_SHAPE_X, INPUT_SHAPE_Y, 1)
-    y_eyes = np.array(y_eyes)
+            img = preprocess_image(img)
+        
+            # check image class
+            image_class = labels.loc[labels['imageid'] == image, 'orbital_tightening']
     
+            if not image_class.empty:
+                image_class = image_class.iloc[0]    
+                
+                # append an equal number of images from each class
+                if (image_class == 0.0 and class_0 < max_images):
+                    x_eyes.append(img)
+                    y_eyes.append(image_class)
+                    class_0 += 1
+                    if (i == max_images*3):
+                        break
+                    i+=1
+                    
+                    
+                elif (image_class == 1.0 and class_1 < max_images):
+                    x_eyes.append(img)
+                    y_eyes.append(image_class)
+                    class_1 += 1
+                    if (i == max_images*3):
+                        break
+                    i+=1
+                    
+                    
+                elif (image_class == 2.0 and class_2 < max_images):
+                    x_eyes.append(img)
+                    y_eyes.append(image_class)
+                    class_2 += 1
+                    if (i == max_images*3):
+                        break
+                    i+=1
+                
+    x_eyes = np.array(x_eyes)
+    y_eyes = np.array(y_eyes)
+    x_eyes = x_eyes.reshape(-1, INPUT_SHAPE_X, INPUT_SHAPE_Y, 1)
     print("Done.\n")
     return x_eyes, y_eyes
     
@@ -83,55 +119,55 @@ def split_data(x_data, y_data):
     x_train, x_val, y_train, y_val = model_selection.train_test_split(
         x_train_val, y_train_val, test_size=0.2, random_state=42)
     
-    
-    # make sure all values are integers
-    x_train = x_train.astype('int32')
-    y_train = y_train.astype('int32')
-    x_val = x_val.astype('int32')
-    y_val = y_val.astype('int32')
-    x_test = x_test.astype('int32')
-    y_test = y_test.astype('int32')
-
-    '''
     # one-hot code labels
     y_train = tf.one_hot(y_train,3)
     y_val = tf.one_hot(y_val,3)
     y_test = tf.one_hot(y_test,3)
-    '''
     
     print("Done.\n")
     return x_train, y_train, x_val, y_val, x_test, y_test
         
-### BUILING & EVALUATING THE MODELS
+### BUILING & EVALUATING THE MODEL
 def create_model():
     print("Creating the model...") 
     model = tf.keras.models.Sequential([
         # convolution layers
-        tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(INPUT_SHAPE_X,INPUT_SHAPE_Y,1)),
+        tf.keras.layers.Conv2D(32, (3,3), activation=tf.keras.layers.LeakyReLU(),  padding='same', 
+                               input_shape=(INPUT_SHAPE_X,INPUT_SHAPE_Y,1)),
+        tf.keras.layers.MaxPooling2D((2,2)),
+        tf.keras.layers.Dropout(0.5),
+        
+        tf.keras.layers.Conv2D(64, (3,3), activation=tf.keras.layers.LeakyReLU(), padding='same'),
+        tf.keras.layers.MaxPooling2D((2,2)),
+        tf.keras.layers.Dropout(0.5),
+        
+        tf.keras.layers.Conv2D(128, (3,3), activation=tf.keras.layers.LeakyReLU(), padding='same'),
         tf.keras.layers.MaxPooling2D((2,2)),
         
-        
-        tf.keras.layers.Conv2D(256, (3,3), activation='relu'),
-        tf.keras.layers.MaxPooling2D((2, 2)),
 
         tf.keras.layers.Flatten(),
         
         # dense layers
-        tf.keras.layers.Dense(64, activation='relu'),
-        tf.keras.layers.Dense(1, activation='linear')
+        tf.keras.layers.Dense(32, activation=tf.keras.layers.LeakyReLU()),
+        tf.keras.layers.Dense(3, activation='softmax')
     ])
     print("Done.\n")   
     return model
 
 # train the model
-def train_model(images, labels, val_images, val_labels):
+def train_model(images, labels, val_images, val_labels, weights):
     model = create_model()
+    
+    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=1e-3,
+                                                                decay_steps=1000,
+                                                                decay_rate=0.9)
     
     # compile the model
     print("Compiling the model...")   
-    model.compile(loss='mse',
-              optimizer='adam',
-              metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy',
+              optimizer=tf.keras.optimizers.SGD(learning_rate=1e-7, momentum=0.9),
+              metrics=['accuracy']
+              )
     
     
     print("Done.\n")   
@@ -142,7 +178,9 @@ def train_model(images, labels, val_images, val_labels):
         images, labels,
         epochs=EPOCHS,
         batch_size = BATCH_SIZE,
-        validation_data=(val_images, val_labels),)
+        validation_data=(val_images, val_labels),
+        #class_weight=weights
+        )
     print("Done.\n")
     
     # save the model
@@ -166,25 +204,123 @@ def load_model():
     print("Done.\n")
     return model
 
-### SCORING EACH CATEGORY   
+def conf_matrix(model, x_test, y_test):
+    from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+    
+    prob_array = model.predict(x_test)
+    class_indices = np.argmax(prob_array, axis=1)
+    print(prob_array)
+    # Convert the class indices to a one-hot encoded array
+    class_indices = np.argmax(prob_array, axis=1)
+    num_classes = 3
+    y_pred = np.zeros((prob_array.shape[0], num_classes))
+    y_pred[np.arange(prob_array.shape[0]), class_indices] = 1
+    
+    print(y_pred)
+    print(np.shape(y_pred))
+    
+    confusion = confusion_matrix(np.asarray(y_test).argmax(axis=1), np.asarray(y_pred).argmax(axis=1))
+    print('Confusion Matrix\n')
+    print(confusion)
+
+    #importing accuracy_score, precision_score, recall_score, f1_score
+    print('\nAccuracy: {:.2f}\n'.format(accuracy_score(y_test, y_pred)))
+
+    print('\nClassification Report\n')
+    print(classification_report(y_test, y_pred, target_names=['Score 0', 'Score 1', 'Score 2']))
+    
+    import seaborn as sns
+
+    lables = ['0','1','2']    
+
+    ax= plt.subplot()
+
+    sns.heatmap(confusion, annot=True, fmt='g', ax=ax);  #annot=True to annotate cells, ftm='g' to disable scientific notation
+
+    # labels, title and ticks
+    ax.set_xlabel('Predicted labels');ax.set_ylabel('True labels'); 
+    ax.set_title('Confusion Matrix'); 
+    ax.xaxis.set_ticklabels(lables); ax.yaxis.set_ticklabels(lables);
+    plt.show()
+
+def save_history(hist):
+    history_df = pd.DataFrame(hist.history)
+    history_df.to_csv('training_history.csv', index=False)
+    
 def eyes():
     # get the data
     x_eyes, y_eyes = load_eyes()
-    print(np.shape(x_eyes), np.shape(y_eyes))
-    '''
+    
     x_train, y_train, x_val, y_val, x_test, y_test = split_data(x_eyes, y_eyes)
 
     # train the model
-    history, model = train_model(x_train, y_train, x_val, y_val)
+    weights = {0: 1., 1: 1., 2: 1}
+    history, model = train_model(x_train, y_train, x_val, y_val, weights)
     print(history)
+    with open('hist.txt', 'w') as f:
+        # Print to the file using the file parameter
+        print(history, file=f)
     
     # evaluate the model
     results = evaluate_model(model, x_test, y_test)
     print(results)
-    '''
+    
+    conf_matrix(model,x_test,y_test)
 
+def eyes_vgg16():
+    # iterate over images
+    eyes_path = 'data/eyes'
+    eyes_dir = os.listdir(eyes_path)
+    x_eyes = []
+    i=0
+    for image in eyes_dir:
+        image_path = os.path.join(eyes_path, image)
+        img = tf.keras.preprocessing.image.load_img(image_path, target_size=(128, 64))
+        x = tf.keras.preprocessing.image.img_to_array(img)
+        x_eyes.append(x)
+        i+=1
+    
+    # load labels
+   
+    labels = load_labels()
+    y_eyes = labels.iloc [:, 2]
+    y_eyes = y_eyes[:8783]
+    print(len(x_eyes))
+    print(len(y_eyes))
+    x_eyes = tf.keras.applications.vgg16.preprocess_input(np.array(x_eyes))
+    
+    x_train, y_train, x_val, y_val, x_test, y_test = split_data(x_eyes, y_eyes)
+    
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.applications.vgg16.VGG16(weights='model-weights/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5', 
+                                                include_top=False,
+                                                input_shape=(INPUT_SHAPE_X, INPUT_SHAPE_Y, 3)))
+    model.add(tf.keras.layers.Flatten(input_shape=model.output_shape[1:]))
+    model.add(tf.keras.layers.Dense(256, activation='relu'))
+    model.add(tf.keras.layers.Dense(128, activation='relu'))
+    model.add(tf.keras.layers.Dense(3, activation='softmax'))
+
+    # Compile the model with appropriate optimizer, loss function, and metrics
+    model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=1e-6), 
+                  loss='categorical_crossentropy', 
+                  metrics=['accuracy'])
+
+    # Train the model on your data
+    model.fit(x_train, y_train, 
+              epochs=EPOCHS,
+              batch_size=BATCH_SIZE, 
+              validation_data=(x_val, y_val))
+    
+    results = model.evaluate(x_test, y_test)
+    print(results)
+    save_history(results)
+    
+    conf_matrix(model,x_test,y_test)
+    
 ### MAIN
 def main():
-    eyes()
+    #eyes()
+    eyes_vgg16()
+    
 
 main()
