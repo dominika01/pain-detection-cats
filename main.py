7b2a6195911bb42b739169bc8942dae0b3210730
sc20dzi <sc20dzi@leeds.ac.uk>
2023-04-12 16:08:48 +0200

    added a feature to read image path from cmd line arguments

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import tensorflow as tf
 import keypoints
 import joblib
+import sys
 
 def preprocess_feature (feature, x_size, y_size):
     resized = cv2.resize(feature, (x_size, y_size))
@@ -21,62 +22,67 @@ def score_feature (feature, model):
 
 # path to the image
 print("Loading the image…")
-image_path = 'data-keypoints/CAT_00/00000001_029.jpg' # change to cmd line arg later?
-image = cv2.imread(image_path)
+#image_path = PATH
+image_path = sys.argv[1]
 
-# crop the features
-print("Cropping the features…")
-ears, eyes, muzzle = keypoints.predict_and_crop(image_path)
-feature_scores = []
+try:
+    image = cv2.imread(image_path)
 
-# classify ears
-print("\nClassifying ears…")
-ears_preprocessed = preprocess_feature(ears, 128, 64)
-ears_model = tf.keras.models.load_model('model-ears')
-ears_score = score_feature (ears_preprocessed, ears_model)
-feature_scores.append(ears_score)
+    # crop the features
+    print("Cropping the features…")
+    ears, eyes, muzzle = keypoints.predict_and_crop(image_path)
+    feature_scores = []
 
-# classify eyes
-print("\nClassifying eyes…")
-eyes_preprocessed = preprocess_feature(eyes, 128, 64)
-eyes_model = tf.keras.models.load_model('model-eyes')
-eyes_score = score_feature (eyes_preprocessed, eyes_model)
-feature_scores.append(eyes_score)
+    # classify ears
+    print("\nClassifying ears…")
+    ears_preprocessed = preprocess_feature(ears, 128, 64)
+    ears_model = tf.keras.models.load_model('model-ears')
+    ears_score = score_feature (ears_preprocessed, ears_model)
+    feature_scores.append(ears_score)
 
-# classify muzzle
-print("\nClassifying muzzle…")
-muzzle_preprocessed = preprocess_feature(muzzle, 128, 64)
-muzzle_model = tf.keras.models.load_model('model-muzzle')
-muzzle_score = score_feature (muzzle_preprocessed, muzzle_model)
-feature_scores.append(muzzle_score)
+    # classify eyes
+    print("\nClassifying eyes…")
+    eyes_preprocessed = preprocess_feature(eyes, 128, 64)
+    eyes_model = tf.keras.models.load_model('model-eyes')
+    eyes_score = score_feature (eyes_preprocessed, eyes_model)
+    feature_scores.append(eyes_score)
 
-# classify whiskers
-print("\nClassifying whiskers…")
-whiskers_preprocessed = preprocess_feature(muzzle, 128, 64)
-whiskers_model = tf.keras.models.load_model('model-whiskers')
-whiskers_score = score_feature (whiskers_preprocessed, whiskers_model)
-feature_scores.append(whiskers_score)
+    # classify muzzle
+    print("\nClassifying muzzle…")
+    muzzle_preprocessed = preprocess_feature(muzzle, 128, 64)
+    muzzle_model = tf.keras.models.load_model('model-muzzle')
+    muzzle_score = score_feature (muzzle_preprocessed, muzzle_model)
+    feature_scores.append(muzzle_score)
 
-# classify head
-print("\nClassifying head…")
-head_preprocessed = preprocess_feature(image, 256, 256)
-head_model = tf.keras.models.load_model('model-head')
-head_score = score_feature (head_preprocessed, head_model)
-feature_scores.append(head_score)
+    # classify whiskers
+    print("\nClassifying whiskers…")
+    whiskers_preprocessed = preprocess_feature(muzzle, 128, 64)
+    whiskers_model = tf.keras.models.load_model('model-whiskers')
+    whiskers_score = score_feature (whiskers_preprocessed, whiskers_model)
+    feature_scores.append(whiskers_score)
 
-# calculate score
-feature_scores = np.array(feature_scores)
-feature_scores = feature_scores.reshape((1, 5))
-model_score = joblib.load('model-score.joblib')
-result = model_score.predict(feature_scores)
-result = np.round(result).astype(int)
-final_score = result[0]
+    # classify head
+    print("\nClassifying head…")
+    head_preprocessed = preprocess_feature(image, 256, 256)
+    head_model = tf.keras.models.load_model('model-head')
+    head_score = score_feature (head_preprocessed, head_model)
+    feature_scores.append(head_score)
 
-# display results
-print("\n\nResults\n")
-print("Ear position\t", ears_score)
-print("Orbital tightening\t", eyes_score)
-print("Muzzle tension\t", muzzle_score)
-print("Whiskers position\t", whiskers_score)
-print("Head position\t", head_score)
-print("\nOverall impression\n", final_score)
+    # calculate score
+    feature_scores = np.array(feature_scores)
+    feature_scores = feature_scores.reshape((1, 5))
+    model_score = joblib.load('model-score.joblib')
+    result = model_score.predict(feature_scores)
+    result = np.round(result).astype(int)
+    final_score = result[0]
+
+    # display results
+    print("\n\nResults\n")
+    print("Ear position\t\t", ears_score)
+    print("Orbital tightening\t", eyes_score)
+    print("Muzzle tension\t\t", muzzle_score)
+    print("Whiskers position\t", whiskers_score)
+    print("Head position\t\t", head_score)
+    print("\nOverall impression\t", final_score)
+except:
+    print("Issue loading image")
